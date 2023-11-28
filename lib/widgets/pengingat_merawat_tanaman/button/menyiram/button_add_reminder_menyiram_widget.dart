cbb3dc5fbe99089f98c2998c17773c044b19a1d6
AmaliaSwastika <swastika.amalia@gmail.com>
2023-11-29 03:19:27 +0700

    Update Bottom Sheet Pengingat Merawat Tanaman

--- lib/widgets/pengingat_merawat_tanaman/button/menyiram/button_add_reminder_menyiram_widget.dart
+++ lib/widgets/pengingat_merawat_tanaman/button/menyiram/button_add_reminder_menyiram_widget.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:capstone_project/data/theme_color.dart';
 import 'package:capstone_project/widgets/pengingat_merawat_tanaman/text_field/text_field_reminder_widget.dart';
-import 'package:flutter/material.dart';
 
 class ButtonAddReminderMenyiram extends StatelessWidget {
   final Function()? onPressed;
@@ -34,64 +34,121 @@ class ButtonAddReminderMenyiram extends StatelessWidget {
     );
   }
 
-  void _showAddReminderMenyiram(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-           children: <Widget>[
-            Row(
+  void _showAddReminderMenyiram(BuildContext context) async {
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  await showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    builder: (context) => Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Batalkan'),
-                ),
-                // const SizedBox(width: 80.0), 
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                child: const Text('Selesai'),
-                ),
-              ],
-            ),
-              
-          const ListTile(
-            title: TextFieldReminderWidget(), 
-          ),
-            ListTile(
-              subtitle: InkWell(
-                onTap: () {
-                  _selectTime(context);
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
                 },
-                child: const Row(
-                  children: <Widget>[
-                    SizedBox(width: 8.0),
-                    Text('Select Time'),
-                  ],
-                ),
+                child: const Text("Batalkan"),
               ),
-            ),
-            // Add more options as needed
-          ],
-        );
-      },
-    );
-  }
+              TextButton(
+                onPressed: () {
+                },
+                child: const Text("Selesai"),
+              ),
+            ],
+          ),
+          const ListTile(
+            title: TextFieldReminderWidget(),
+          ),
+          ListTile(
+            title: const Text("Pilih Waktu"),
+            trailing: const Icon(Icons.access_time),
+            onTap: () async {
+              TimeOfDay? pickedTime = await showTimePicker(
+                context: context,
+                initialTime: selectedTime,
+              );
 
-  Future<void> _selectTime(BuildContext context) async {
-    TimeOfDay? pickedTime = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-    );
-
-    if (pickedTime != null) {
-      // Do something with the picked time
-      print('Selected Time: ${pickedTime.format(context)}');
-    }
-  }
+              if (pickedTime != null && pickedTime != selectedTime) {
+                selectedTime = pickedTime;
+              }
+            },
+          ),
+        ],
+      ),
+    ),
+  );
 }
+}
+
+
+//   void _showAddReminderMenyiram(BuildContext context) {
+//     showModalBottomSheet(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return Column(
+//           mainAxisSize: MainAxisSize.min,
+//            children: <Widget>[
+//             Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 TextButton(
+//                   onPressed: () {
+//                     Navigator.of(context).pop();
+//                   },
+//                   child: const Text('Batalkan'),
+//                 ),
+//                 // const SizedBox(width: 80.0), 
+//                 TextButton(
+//                   onPressed: () {
+//                     Navigator.of(context).pop();
+//                   },
+//                 child: const Text('Selesai'),
+//                 ),
+//               ],
+//             ),
+              
+//           const ListTile(
+//             title: TextFieldReminderWidget(), 
+//           ),
+//           // const ListTile(
+//           //   title: NumberPage(), 
+//           // ),
+//             ListTile(
+//               subtitle: InkWell(
+//                 onTap: () {
+//                   _selectTime(context);
+//                 },
+//                 child: const Row(
+//                   children: <Widget>[
+//                     SizedBox(width: 8.0),
+//                     Text('Select Time'),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//             //Add more options as needed
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+
+//   Future<void> _selectTime(BuildContext context) async {
+//     TimeOfDay? pickedTime = await showTimePicker(
+//       context: context,
+//       initialTime: TimeOfDay.now(),
+//     );
+
+//     if (pickedTime != null) {
+//       // Do something with the picked time
+//       print('Selected Time: ${pickedTime.format(context)}');
+//     }
+//   }
+// }
