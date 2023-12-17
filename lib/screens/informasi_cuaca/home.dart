e2e181eb3d4127ce76ef2ce3a7a92c0d03b00423
AmaliaSwastika <swastika.amalia@gmail.com>
2023-12-18 02:37:43 +0700

    Update Get API Weather Detail

--- lib/screens/informasi_cuaca/home.dart
+++ lib/screens/informasi_cuaca/home.dart
@@ -1,6 +1,7 @@
 import 'package:capstone_project/data/home_text_style.dart';
 import 'package:capstone_project/screens/implementasi_ai/chatbot/first_screen_chat_bot.dart';
 import 'package:capstone_project/screens/informasi_cuaca/detail_cuaca.dart';
+import 'package:capstone_project/screens/informasi_cuaca/weather_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
@@ -165,7 +166,7 @@ class _HomeState extends State<Home> {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const DetailCuaca(),
+                            builder: (context) => const WeatherDetail(),
                           ),
                         );
                       },
--- lib/widgets/pengingat_merawat_tanaman/text_field/search_reminder_widget.dart
+++ lib/widgets/pengingat_merawat_tanaman/text_field/search_reminder_widget.dart
@@ -2,6 +2,7 @@ import 'package:capstone_project/providers/plant_reminder_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+// ignore: use_key_in_widget_constructors
 class SearchAllPlants extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,8 +18,26 @@ class SearchAllPlants extends StatelessWidget {
             decoration: InputDecoration(
               hintText: 'Cari Tanaman',
               prefixIcon: const Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12.0),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25.0),
+                borderSide: const BorderSide(
+                  color: Colors.black, // Set the border color to black
+                  width: 1.5,           // Set the border width
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25.0),
+                borderSide: const BorderSide(
+                  color: Colors.black, // Set the border color to black
+                  width: 1.5,           // Set the border width
+                ),
+              ),
+              disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25.0),
+                borderSide: const BorderSide(
+                  color: Color.fromARGB(255, 0, 0, 0), // Set the border color for disabled state
+                  width: 1.5,           // Set the border width for disabled state
+                ),
               ),
             ),
           ),
