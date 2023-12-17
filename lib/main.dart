40b52cadec9f0eab5340156b0dd07f78994728c7
AmaliaSwastika <swastika.amalia@gmail.com>
2023-12-17 17:50:09 +0700

    Fixed Latlong

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:capstone_project/providers/auth_provider.dart';
 import 'package:capstone_project/providers/plant_provider.dart';
 import 'package:capstone_project/providers/plant_reminder_provider.dart';
 import 'package:capstone_project/screens/app_opening_screens/app_desc_screen.dart';
-import 'package:capstone_project/screens/menanam_tanaman_screens/menanam_screen.dart';
-import 'package:capstone_project/screens/navigation_bar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -43,7 +41,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const NavBar(),
+        home: const AppDescScreen(),
       ),
     );
   }
